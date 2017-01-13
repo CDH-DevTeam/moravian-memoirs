2f49244bfcd11c1e74f3648f60bb77aef0a18051
traustid <traustid@gmail.com>
2017-01-13 14:00:03 +0100

    Delete config.js

--- www/js/config.js
+++
@@ -1,4 +0,0 @@
-window.mediaWikiUrl = 'http://localhost/mediawiki_test/api.php';
-//window.apiUrl = 'http://localhost/moravian-api/';
-window.apiUrl = 'http://moravianlives.org:8001';
-// 'http://moravianlives.org/transcriptions/api.php'
