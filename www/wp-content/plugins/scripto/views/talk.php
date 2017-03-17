553645f8628cbb5fee0c205d12779e375b93b010
traustid <traustid@gmail.com>
2017-03-17 11:57:00 +0100

    Fixed layout errors in discussion page.

--- www/wp-content/plugins/scripto/views/talk.php
+++ www/wp-content/plugins/scripto/views/talk.php
@@ -1,24 +1,37 @@
-<div id="scripto">
+<div id="scripto" class="scripto scripto-transcription side-view">
 
 <?php echo $this->get_navigation(); ?>
 
-<h2>Discuss <cite><?php echo $doc->getPageName(); ?></cite></h2>
-<h3>in <cite><?php echo $doc->getTitle(); ?></cite></h3>
+<div class="transcription-wrapper">
 
-<div><?php echo $this->get_media_viewer( $_GET['scripto_doc_page_id'] ); ?></div>
+	<div class="transcription-header">
+		<h3>Discuss <cite><?php echo $doc->getPageName(); ?></cite></h3>
+		<h4>in <cite><?php echo $doc->getTitle(); ?></cite></h4>
+	</div>
 
-<?php if ( $doc->canEditTalkPage() ): ?>
-<form action="" method="post">
-	<textarea name="scripto_talk" cols="45" rows="12"><?php echo $doc->getTalkPageWikitext(); ?></textarea>
-	<input type="submit" name="scripto_submit_talk" value="Save discussion" />
-</form>
-<?php else: ?>
-<p>You don't have permission to discuss this page.</p>
-<?php endif; ?>
+	<div class="transcription-container">
+		<div class="image-wrapper">
+			<?php echo $this->get_media_viewer( $_GET['scripto_doc_page_id'] ); ?>
+		</div>
 
-<p><a href="<?php echo $url_transcription; ?>">transcribe page</a> | <a href="<?php echo $url_talk_history; ?>">view history</a> | <a href="<?php echo $url_document; ?>">view document</a></p>
+		<div class="text-input-wrapper">
+			<?php if ( $doc->canEditTalkPage() ): ?>
+				<form action="" method="post">
+					<textarea name="scripto_talk" cols="45" rows="12"><?php echo $doc->getTalkPageWikitext(); ?></textarea>
+					<input type="submit" name="scripto_submit_talk" value="Save discussion" />
+				</form>
+			<?php else: ?>
+				<p>You don't have permission to discuss this page.</p>
+			<?php endif; ?>
+		</div>
+	</div>
 
-<h2>Current Discussion</h2>
-<div><?php echo $doc->getTalkPageHtml(); ?></div>
+
+	<p><a href="<?php echo $url_transcription; ?>">transcribe page</a> | <a href="<?php echo $url_talk_history; ?>">view history</a> | <a href="<?php echo $url_document; ?>">view document</a></p>
+
+	<h2>Current Discussion</h2>
+	<div><?php echo $doc->getTalkPageHtml(); ?></div>
+
+</div>
 
 </div>
